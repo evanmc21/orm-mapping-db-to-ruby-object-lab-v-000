f660f46f2fd4897c44227c3db89c03176a58aca3
Evan Candler <emcandler21@gmail.com>
2018-04-02 22:14:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,10 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
-    "SELECT stuendts.id FROM students, COUNT(student.id) WHERE grade = '9'"
+    sql = <<-SQL
+      SELECT COUNT(grade) FROM students WHERE grade = 9
+      SQL
+      DB[:conn].execute(sql)
   end
 
   def save
